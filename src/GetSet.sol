3605b21cffd2871f67d0e0d48fffe64ce44082e2
SchneiderNejc <nejc.sch@gmail.com>
2022-09-23 22:30:58 +0200

    GetSet.sol: add array with sample values

--- src/GetSet.sol
+++ src/GetSet.sol
@@ -1,13 +1,21 @@
 pragma solidity 0.8.15;
 
 contract GetSet {
+
     bool value;
+    uint public identifier = 10;
+    uint[] public values = [identifier, 4, 3, 2, 1];
 
     function getValue() external view returns(bool) {
         return value;
     }
 
     function setValue(bool _value) external {
+        require(value != _value, "cant set same value");
         value = _value;
+    }
+
+    function getValues() external view returns(uint[] memory) {
+        return values;
     }
 }
