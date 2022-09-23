5c7578f3f105346dcded8884223223a08219d31d
SchneiderNejc <nejc.sch@gmail.com>
2022-09-23 22:32:27 +0200

    GetSet.t.sol: add tests for emitting events and consoleLogging

--- test/GetSet.t.sol
+++ test/GetSet.t.sol
@@ -7,11 +7,42 @@ contract GetSetTest is Test {
     // contract instances
     GetSet getSet;
 
+    event LogValues(bool value, uint identifier);
+    event ValuesArray(uint[] values);
+
+    //--------------------------------------------------------------------------
+    // setUp, test and testCannot
+
     function setUp() public {
         getSet = new GetSet();
     }
 
     function testValueIsFalse() public {
         assertFalse(getSet.getValue());
+    }
+
+    function testCannotSetSameValue() public {
+        vm.expectRevert(bytes("cant set same value"));
+        getSet.setValue(false);
+    }
+
+    //--------------------------------------------------------------------------
+    // console.log
+
+    function testConsoleLogValues() public {
+        bool value = getSet.getValue();
+        console.log("value: %s identifier: %s", value, getSet.identifier());
+    }
+
+    //--------------------------------------------------------------------------
+    // events emitting
+
+    function testEmitValues() public {
+        emit LogValues(getSet.getValue(), getSet.identifier());
+    }
+
+    function testEmitArray() public {
+        uint[] memory data = getSet.getValues();
+        emit ValuesArray(data);
     }
 }
