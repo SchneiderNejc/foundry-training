72ee52bcb33e5f1e2eec1bd4ba0254cffd3a54eb
SchneiderNejc <nejc.sch@gmail.com>
2022-09-25 19:08:27 +0200

    GetSet.t.sol: add skip/rewind and prank/hoax tests

--- test/GetSet.t.sol
+++ test/GetSet.t.sol
@@ -29,14 +29,13 @@ contract GetSetTest is Test {
     //--------------------------------------------------------------------------
     // console.log
 
-    function testConsoleLogValues() public {
+    function testConsoleLogValues() public view {
         bool value = getSet.getValue();
         console.log("value: %s identifier: %s", value, getSet.identifier());
     }
 
     //--------------------------------------------------------------------------
-    // events emitting
-
+    // std logs
     function testEmitValues() public {
         emit LogValues(getSet.getValue(), getSet.identifier());
     }
@@ -45,4 +44,44 @@ contract GetSetTest is Test {
         uint[] memory data = getSet.getValues();
         emit ValuesArray(data);
     }
+
+    //--------------------------------------------------------------------------
+    // std assertions
+
+    // TODO
+
+    //--------------------------------------------------------------------------
+    // std cheats
+
+    // skip rewind
+    function testOnlyInFuture() public {
+        assertFalse(getSet.onlyInFuture());
+
+        skip(60);
+        assertTrue(getSet.onlyInFuture());
+
+        rewind(45);
+        assertFalse(getSet.onlyInFuture());
+    }
+
+    // prank, hoax
+    function testOnlyOwner() public {
+        assertFalse(getSet.onlyOwner());
+
+        //vm.prank only works for the next call
+        vm.prank(0xE71d14a3fA97292BDE885C1D134bE4698e09b3B7);
+        assertTrue(getSet.onlyOwner());
+        assertFalse(getSet.onlyOwner());
+
+        // multiple return value
+        address sender;
+        address owner;
+        (sender, owner) = getSet.getSenderOwner();
+        console.log("sender: %s :: owner: $s", sender, owner);
+    }
+
+
+
+
+
 }
