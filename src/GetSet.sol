47e12e5c728abb1cd37a3dea97b14c55f57440d7
SchneiderNejc <nejc.sch@gmail.com>
2022-09-25 19:07:37 +0200

    GetSet.sol: add onlyInFuture(), ownable methods

--- src/GetSet.sol
+++ src/GetSet.sol
@@ -5,6 +5,8 @@ contract GetSet {
     bool value;
     uint public identifier = 10;
     uint[] public values = [identifier, 4, 3, 2, 1];
+    uint timeCreated = block.timestamp;
+    address owner = 0xE71d14a3fA97292BDE885C1D134bE4698e09b3B7;
 
     function getValue() external view returns(bool) {
         return value;
@@ -18,4 +20,19 @@ contract GetSet {
     function getValues() external view returns(uint[] memory) {
         return values;
     }
+
+    // returns true if 30 seconds after contract created
+    function onlyInFuture() external view returns(bool) {
+        return block.timestamp > timeCreated + 30;
+    }
+
+    function onlyOwner() external view returns(bool) {
+        return msg.sender == owner;
+    }
+
+    function getSenderOwner() external view returns(address, address) {
+        return (msg.sender, owner);
+    }
+
+
 }
