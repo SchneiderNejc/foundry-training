5a02c85e7227768d88436f9ff7b33e09b67a5a35
SchneiderNejc <nejc.sch@gmail.com>
2022-09-26 13:10:39 +0200

    GetSet.t.sol: add tests for deployAdditionalContract

--- test/GetSet.t.sol
+++ test/GetSet.t.sol
@@ -64,7 +64,7 @@ contract GetSetTest is Test {
         assertFalse(getSet.onlyInFuture());
     }
 
-    // prank, hoax
+    // prank
     function testOnlyOwner() public {
         assertFalse(getSet.onlyOwner());
 
@@ -79,6 +79,41 @@ contract GetSetTest is Test {
         (sender, owner) = getSet.getSenderOwner();
         console.log("sender: %s :: owner: $s", sender, owner);
     }
+
+    // ERC20 methods
+    // hoax -> prank from funded address,
+    // startHoax ->  perpetual prank from funded address,
+    // deal -> mint erc20
+
+    // deployCode
+    function testCannotDeployCodeWithInvalidParams() public {
+        vm.expectRevert(bytes("number cant be 0"));
+        deployCode("AdditionalDeployment.sol",
+            abi.encode(0, 0xE71d14a3fA97292BDE885C1D134bE4698e09b3B7));
+
+
+        vm.expectRevert(bytes("invalid address"));
+        deployCode("AdditionalDeployment.sol",
+            abi.encode(5, address(0)));
+    }
+
+    function testDeployCode() public {
+        // deploy additional contract and save its address
+        address additionalDeployment = deployCode("AdditionalDeployment.sol",
+            abi.encode(5, 0xE71d14a3fA97292BDE885C1D134bE4698e09b3B7));
+        console.log("additionalDeployment: %s", additionalDeployment);
+
+        // retrieve stored address from newly deployed contract
+        (bool success, bytes memory data) = additionalDeployment.call(
+            abi.encodeWithSignature("getOwner()"));
+        assertTrue(success);
+
+        // extract return value from .call
+        address owner = abi.decode(data, (address));
+        console.log("newly deployed contract owner: $s", owner);
+    }
+
+
 
 
 
